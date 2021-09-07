6ed83ece71c3ce8d24ed261a0c80a02a22052c99
CorkyRapier <worldEGORa@gmail.com>
2021-09-08 02:25:39 +0300

    first parser

---
+++ so_simply_parser.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://habr.com/ru/search/?q=python&target_type=posts&order=date"
+
+request = requests.get(url)
+
+soup = BeautifulSoup(request.text, "html.parser")
+
+teme = soup.find_all("h2", class_="tm-article-snippet__title tm-article-snippet__title_h2")
+
+# link = soup.find_all('a', class_="tm-article-snippet__title-link")
+
+for temes in teme:
+    temes = temes.find('a')
+    if temes is not None:
+        sublink = temes.get('href')
+        print(str(temes.text) + ' ' + 'https://habr.com' + str(sublink))
+        print('-------------------------------------------------')
